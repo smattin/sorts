56d681e4394d7a379b408e76a1a53bb9feb9297c
Stephen Mattin <stephen@mattin.net>
2021-04-01 07:56:35 -0400

    delete unused

--- Swaps.java
+++ Swaps.java
@@ -12,36 +12,6 @@ public class Swaps {
 
     static java.util.logging.Logger debug = java.util.logging.Logger.getGlobal();
 
-    /* see if given strings contain the same chars
-    static boolean anagrammatic(String s1, String s2) {
-        if(s1.length() != s2.length()) {
-            return false;
-        }
-
-        char c1[] = s1.toCharArray();
-        char c2[] = s2.toCharArray();
-        Arrays.sort(c1);
-        Arrays.sort(c2);
-        return Arrays.equals(c1,c2);
-    }
-
-    static List<String> substrings(String s) {
-        int end = s.length();
-        List<String> sss = new ArrayList<String>();
-
-        for (int i=0; i<=end; i++) {
-            for (int j=i+1; j<=end; j++) {
-		String sub = s.substring(i,j);
-                sss.add(sub);
-	        // Anna.debug.log(java.util.logging.Level.SEVERE,String.format("%s %d %d",sub,i,j));
-            }
-        }
-
-	sss.remove(s); // strict subsets
-        return sss;
-    }
-    */
-
     // Complete the minimumSwaps function below.
     // https://www.hackerrank.com/challenges/minimum-swaps-2/problem
     public static int minimumSwaps(int[] arr) {
